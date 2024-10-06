7b4790f28f6ca5292c82c0fa1eb5b0abcb859655
RaphMun <151914168+RaphMun@users.noreply.github.com>
2024-10-06 20:11:31 +0200

    Add files via upload
    Fin TP 1

--- src/main/java/appli/todolistjx/StartApplication.java
+++ src/main/java/appli/todolistjx/StartApplication.java
@@ -1,23 +1,40 @@
 package appli.todolistjx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StartApplication extends Application {
+    private static Stage mainStage;
     @Override
     public void start(Stage stage) throws IOException {
+        this.mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        this.mainStage.setTitle("Hello!");
+        this.mainStage.setScene(scene);
+        this.mainStage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void changeScene(String nomDuFichierFxml) {
+        try {
+            Parent root = FXMLLoader.load(StartApplication.class.getResource(nomDuFichierFxml));
+            mainStage.setScene(new Scene(root));
+            mainStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void onHelloButtonClick(ActionEvent actionEvent) {
     }
 }
---
+++ src/main/java/appli/todolistjx/acceuil/InscriptionController.java
@@ -0,0 +1,8 @@
+package appli.todolistjx.acceuil;
+
+import javafx.event.ActionEvent;
+
+public class InscriptionController {
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
+}
